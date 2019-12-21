8c03dab01fa482d6adb3f268da72bd841df8444b
Mikael Kaya <kayaswork@gmail.com>
2019-12-21 23:28:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,10 @@
-def oxford_comma
-  ["fiddleheads","okra","kohlrabi"]
-  ["fiddleheads","okra","kohlrabi"].join
+def oxford_comma(array)
+  if array.length == 1
+    return array.join
+  elsif array.length == 2 
+    return array.join(" and ")
+  else array.length >= 3
+    array[-1] << " and #{array[-1]}"
+    return array.join(", ")
+  end
 end
