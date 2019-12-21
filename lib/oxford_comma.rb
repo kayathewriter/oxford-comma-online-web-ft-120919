a17312a0076513bf212f072308715f8b420fa20c
Mikael Kaya <kayaswork@gmail.com>
2019-12-21 23:39:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,17 @@
-def oxford_comma(array)
-  if array.length == 1
-    return array.join
-  elsif array.length == 2 
-    return array.join(" and ")
-  else array.length >= 3
-    array[-1] << " and #{array[+1]}"
-    return array.join(", ")
-  end
+ def oxford_comma(array)
+  if array.size == 1
+    return array[0]
+  elsif   array.size == 2
+  two = array.join(" and ")
+  two
+elsif array.size > 2
+  index = array.size - 1
+  save = array[index]
+  comma = array.pop
+  comma1 = array.join(", ")
+  slick = save.to_s
+  comma2 = comma1 << ", and "
+  comma3 = comma2 << slick
+  comma3
+end
 end
